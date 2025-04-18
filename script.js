79f4c82b777bddf6c5afc47447ba067e93d46f39
gabsjk <gabyfranca820@gmail.com>
2025-04-18 13:52:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-
-const card = document.getElementById('card');
+const envelope = document.querySelector('.envelope');
+const card = document.querySelector('.card');
 const music = document.getElementById('music');
 let opened = false;
 
-card.addEventListener('click', () => {
+// Função para abrir e fechar a carta
+envelope.addEventListener('click', () => {
   if (!opened) {
-    card.classList.add('opened');
+    card.style.display = 'flex';
+    envelope.style.transform = 'rotateX(-180deg)';
     music.currentTime = 33;
     music.play();
     opened = true;
   } else {
-    card.classList.remove('opened');
+    card.style.display = 'none';
+    envelope.style.transform = 'rotateX(0deg)';
     music.pause();
-    music.currentTime = 33;
+    music.currentTime = 33; // Reseta o tempo para o início
     opened = false;
   }
 });
 
-const bg = document.getElementById('animated-bg');
-const icons = ['star', 'heart', 'flower', 'flower2', 'heart2', 'star2'];
+// Função para gerar os elementos flutuantes no fundo
+const bg = document.querySelector('body');
+const icons = ['star', 'heart', 'flower'];
 
 for (let i = 0; i < 40; i++) {
   const el = document.createElement('div');
